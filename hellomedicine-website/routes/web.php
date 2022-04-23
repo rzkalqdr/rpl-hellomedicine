d2b8b04c734850d8d1203db3779d73cfafd1548e
Rizki <rzkalqadri@student.telkomuniversity.ac.id>
2022-04-23 13:06:26 +0700

    dashboard rumah sakit (view data only)

--- hellomedicine-website/routes/web.php
+++ hellomedicine-website/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\RumahSakitController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,11 @@ Route::get('/', function () {
 Route::get('artikel', function () {
     return view('artikel');
 });
+
+Route::get('admin/rumahsakit', [RumahSakitController::class, 'index']);
+
+// Route::get('admin/user', [UserController::index]);
+
+// Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
